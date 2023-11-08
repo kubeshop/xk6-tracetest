b483fc4ae29c45defe0c0f4ebcac92575d5efab0
Oscar Reyes <oscar-rreyes1@hotmail.com>
2023-11-08 12:32:25 -0600

    feat: Adding Cloud Support

---
+++ examples/test-from-id-queue-cloud.js
@@ -0,0 +1,42 @@
+import { Http, Tracetest } from "k6/x/tracetest";
+import { sleep } from "k6";
+
+export const options = {
+  vus: 1,
+  duration: "5s",
+};
+
+const http = new Http();
+const testId = "c80sJ_4SR";
+const tracetest = Tracetest();
+tracetest.updateFromConfig({
+  server_url: "https://app.tracetest.io",
+  api_token: "tttoken_21a387236e892554",
+});
+
+export default function () {
+  const url = "http://localhost:8081/pokemon?take=5";
+  const response = http.get(url);
+
+  tracetest.runTest(
+    response.trace_id,
+    {
+      test_id: testId,
+      variable_name: "TRACE_ID",
+      should_wait: true,
+    },
+    {
+      id: "123",
+      url,
+      method: "GET",
+    }
+  );
+
+  sleep(1);
+}
+
+export function handleSummary() {
+  return {
+    stdout: tracetest.summary(),
+  };
+}
